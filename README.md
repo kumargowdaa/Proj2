08704f28367119c5276e4af8ef57792dd227f70e
kumar g <kumar.1rn12is403@gmail.com>
2020-04-12 21:17:40 +0530

    changing project name

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# Proj1
+# Proj2
 
 This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 9.1.0.
 
--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -10,7 +10,7 @@ describe('workspace-project App', () => {
 
   it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getTitleText()).toEqual('Proj1 app is running!');
+    expect(page.getTitleText()).toEqual('Proj2 app is running!');
   });
 
   afterEach(async () => {
